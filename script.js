7a34c50c1a1e63d3fce0db9f1786a100ea99d4a7
psijamartinez <psi.jamartinez@gmail.com>
2026-02-02 00:28:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
-function go(id) {
-  document.querySelectorAll('.node').forEach(n => n.classList.add('hidden'));
-  document.getElementById(id).classList.remove('hidden');
+function show(id) {
+  document.querySelectorAll('.node')
+    .forEach(n => n.classList.remove('active'));
+  document.getElementById(id)
+    .classList.add('active');
 }
